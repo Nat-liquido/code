4854d91db63bd1ada809aeb859779f3288b317c8
Gervásio Araújo <gervasioaraujo@liquido.com>
2022-08-03 17:22:08 -0300

    improving the way to show the payment methods in page template

--- Liquido/PayIn/Block/PaymentMethodsList.php
+++ Liquido/PayIn/Block/PaymentMethodsList.php
@@ -10,13 +10,8 @@ class PaymentMethodsList extends Template
 
     public function getLiquidoBrazilPayInMethods()
     {
-        $brazil_payin_methods = ["PIX", "Boleto", "Cartão de Crédito", "AME Digital", "Pic Pay", "PayPal", "Mercado Pago"];
-        $html_output = "";
-        foreach ($brazil_payin_methods as $payin_method) {
-            $html_output .= "<li><a href='https://www.w3schools.com'>" . $payin_method ."</a></li>";
-        }
-        $html_output = "<ul>" . $html_output . "</ul>";
-        return $html_output;
+        $brazil_payin_methods = ["PIX", "Boleto", "Cartão de Crédito", "AME Digital", "PicPay", "PayPal", "Mercado Pago"];
+        return $brazil_payin_methods;
     }
     
 }
