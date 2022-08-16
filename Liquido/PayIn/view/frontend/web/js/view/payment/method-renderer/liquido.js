a702e3adcf66237895d2f88db32727f39eebc76e
Nat-liquido <nataliasantinello@liquido.com>
2022-08-16 12:45:49 -0300

    Redirecting checkout to Liquido Payments page.

--- Liquido/PayIn/view/frontend/web/js/view/payment/method-renderer/liquido.js
+++ Liquido/PayIn/view/frontend/web/js/view/payment/method-renderer/liquido.js
@@ -1,14 +1,21 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/view/payment/default',
+        'mage/url'
     ],
-    function (Component) {
+    function (quote, Component, urlBuilder) {
         'use strict';
 
         return Component.extend({
             defaults: {
+                redirectAfterPlaceOrder: false,
                 template: 'Liquido_PayIn/payment/form'
-            }
+            },
+            afterPlaceOrder: function () {
+                var url = urlBuilder.build("liquido/checkout/start");
+                window.location.href = url;
+            },
         });
     }
 );
