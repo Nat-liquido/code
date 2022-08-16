c2e9268bcaac4261cc70a7c3fa8eefd5efb5bdb3
Gervásio Araújo <gervasioaraujo@liquido.com>
2022-08-16 15:03:51 -0300

    updating the pix generation in virgo api after save order before

--- Liquido/PayIn/Block/PixPayInStepTwo.php
+++ Liquido/PayIn/Block/PixPayInStepTwo.php
@@ -4,7 +4,6 @@ namespace Liquido\PayIn\Block;
 
 use \Magento\Framework\View\Element\Template;
 use \Magento\Framework\View\Element\Template\Context;
-use \Magento\Framework\App\ObjectManager;
 use \Magento\Checkout\Model\Session;
 
 use \Liquido\PayIn\Service\LiquidoPixPayInService;
@@ -32,50 +31,15 @@ class PixPayInStepTwo extends Template
         $this->checkoutSession = $checkoutSession;
         $this->orderData = $orderData;
 
-        $objectManager = ObjectManager::getInstance();
-        $this->cart = $objectManager->get('\Magento\Checkout\Model\Cart');
-
+        // init the PIX generation in Liquido Virgo API
+        $this->orderId = $this->getIncrementId();
         $customerEmail = $this->getCustomerEmail();
-
-        $amountTotal = $this->getCartAmountTotal();
-        if ($amountTotal == 0) {
+        $grandTotal = $this->getGrandTotal($this->orderId);
+        if ($grandTotal == 0) {
             $this->errorMsg = "O valor da compra deve ser maior que R$0,00.";
             return null;
         }
-
-        $this->orderId = $this->reserveIncrementId();
-
-        $this->pixCode = $pixPayInService->createLiquidoPixPayIn($this->orderId, $customerEmail, $amountTotal);
-
-        $newOrderData = $this->getNewOrderData();
-        try {
-            $this->orderData->createMageOrder($newOrderData);
-        } catch (\Exception $e) {
-            echo $e;
-        }
-    }
-
-    private function getNewOrderData()
-    {
-        $orderData = [
-            'reserved_increment_id'  => $this->orderId,
-            'currency_id'  => 'BRL',
-            'email'        => $this->getCustomerEmail(), //buyer email id
-            'shipping_address' => [
-                'firstname'    => 'John', //address Details
-                'lastname'     => 'Doe',
-                'street' => '123 Demo',
-                'city' => 'Mageplaza',
-                'country_id' => 'US',
-                'region_id' => 12,
-                'postcode' => '10019',
-                'telephone' => '0123456789',
-                'fax' => '32423',
-                'save_in_address_book' => 1
-            ],
-            'items' => $this->getCartItems()
-        ];
-        return $orderData;
+        $this->pixCode = $pixPayInService->createLiquidoPixPayIn($this->orderId, $customerEmail, $grandTotal);
     }
 
     public function getPixCode()
@@ -98,54 +62,25 @@ class PixPayInStepTwo extends Template
         return $this->getRequest()->getParam('email_address');
     }
 
-    private function reserveIncrementId()
+    private function getIncrementId()
     {
         try {
-
-            $this->checkoutSession->getQuote()->reserveOrderId();
-            $orderId = $this->checkoutSession->getQuote()->getReservedOrderId();
-            return $orderId;
+            $orderObj = $this->checkoutSession->getLastRealOrder();
+            return $orderObj->getIncrementId();
         } catch (\Exception $e) {
             echo $e;
             return null;
         }
     }
 
-    private function getCartAmountTotal()
+    private function getGrandTotal($incrementId)
     {
         try {
-            // $objectManager = ObjectManager::getInstance();
-            // $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
-            return (float) $this->cart->getQuote()->getGrandTotal() * 100;
+            return (float) $this->orderData->getOrderGrandTotalFromDatabaseByIncrementId($incrementId) * 100;
         } catch (\Exception $e) {
             echo $e;
             return null;
         }
     }
 
-    private function getCartItems()
-    {
-        try {
-            // $objectManager = ObjectManager::getInstance();
-            // $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
-            // get quote items array
-            return $this->cart->getQuote()->getAllItems();
-        } catch (\Exception $e) {
-            echo $e;
-            return null;
-        }
-    }
-
-    public function getShippingAddress()
-    {
-        try {
-            // $objectManager = ObjectManager::getInstance();
-            // $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
-            // return $cart->getQuote()->getShippingAddress();
-            return $this->cart->getQuote()->getShippingAddress();
-        } catch (\Exception $e) {
-            echo $e;
-            return null;
-        }
-    }
 }
