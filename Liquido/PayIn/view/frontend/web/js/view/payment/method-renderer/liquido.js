47dd5b1578537a3e554adb22a13e558524d3a35f
Nat-liquido <nataliasantinello@liquido.com>
2022-08-16 12:24:14 -0300

    Adding Liquido Payments option in checkout page. Adding Liquido Payment configs to the Payment Methods menu.

---
+++ Liquido/PayIn/view/frontend/web/js/view/payment/method-renderer/liquido.js
@@ -0,0 +1,14 @@
+define(
+    [
+        'Magento_Checkout/js/view/payment/default'
+    ],
+    function (Component) {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                template: 'Liquido_PayIn/payment/form'
+            }
+        });
+    }
+);
