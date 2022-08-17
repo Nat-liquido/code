4082beb26f2963cca19ecd3e1e700e3ce4e20540
Gervásio Araújo <gervasioaraujo@liquido.com>
2022-08-17 10:27:14 -0300

    displaying the loading effect until open the modulo page

--- Liquido/PayIn/view/frontend/web/js/view/payment/method-renderer/liquido.js
+++ Liquido/PayIn/view/frontend/web/js/view/payment/method-renderer/liquido.js
@@ -2,9 +2,10 @@ define(
     [
         'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/view/payment/default',
-        'mage/url'
+        'mage/url',
+        'jquery',
     ],
-    function (quote, Component, urlBuilder) {
+    function (quote, Component, urlBuilder, jQuery) {
         'use strict';
 
         return Component.extend({
@@ -13,6 +14,7 @@ define(
                 template: 'Liquido_PayIn/payment/form'
             },
             afterPlaceOrder: function () {
+                jQuery("body").trigger("processStart");
                 var url = urlBuilder.build("liquido/checkout/start");
                 window.location.href = url;
             },
